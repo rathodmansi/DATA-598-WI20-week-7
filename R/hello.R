45948717b4cdca26f6b8edbf7dba47b11b084732
Mansi Rathod <mrathod@uw.edu>
2020-03-02 13:20:15 -0800

    Deleting the hello.rd from man folder

--- R/hello.R
+++ R/hello.R
@@ -1,26 +1,32 @@
 
-#' Title
+#' Title: Add two numbers function
 #'
-#' @param a
-#' @param b
+#' @param a First Number
+#' @param b Second Number
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' add_two_numbers(2, 4)
+#' add_two_numbers(7, -4)
 add_two_numbers <- function(a, b) {
   return(a+b)
 }
 
-#' Title
+#' Title: Multiply two numbers function
 #'
-#' @param a
-#' @param b
+#' @param a First Number
+#' @param b Second Number
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' multiply_two_numbers(2,5)
+#' multiply_two_numbers(2,1)
+#
 multiply_two_numbers <- function(a, b) {
   return(a*b)
+
 }
