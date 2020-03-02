ea657775f97e5db26d713acc2755bf74ebb23c3f
Mansi Rathod <mrathod@uw.edu>
2020-03-02 12:42:05 -0800

    Updated the hello.R

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -15,4 +15,35 @@
 
 hello <- function() {
   print("Hello, world!")
+}
+
+#' Function to add two numbers
+#'
+#' @param a First number
+#' @param b Second number
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' add_two_numbers(2, 4)
+#' add_two_numbers(7, -4)
+#'
+add_two_numbers <- function(a, b) {
+  return(a+b)
+}
+
+#' Function to multiply two numbers
+#'
+#' @param a First Number
+#' @param b Second Number
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' multiply_two_numbers(2, 4)
+#' multiply_two_numbers(7, -4)
+multiply_two_numbers <- function(a, b) {
+  return(a*b)
 }
