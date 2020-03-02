d9b3ebb21728f647d4c03e09b923e1a20f82734f
Mansi Rathod <mrathod@uw.edu>
2020-03-02 12:50:42 -0800

    Updating the test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(mysimplecompendium22)
+
+test_check("mysimplecompendium22")
---
+++ tests/testthat/test-hello.R
@@ -0,0 +1,19 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("addition works", {
+  expect_equal(add_two_numbers(2, 4), 6)
+})
+
+test_that("addition works", {
+  expect_equal(add_two_numbers(7, -4), 3)
+})
+
+test_that("multiplication works", {
+  expect_equal(2 * 4, 8)
+})
+
+test_that("multiplication works", {
+  expect_equal(7 * -4, -28)
+})
